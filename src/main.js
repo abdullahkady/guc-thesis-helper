db246fb7d89c4ba773a8a2e3dc0941c35e126c8d
Abdullah Kady <abdullahkady@gmail.com>
2019-01-11 21:56:44 +0200

    Add payload formatter

--- src/main.js
+++ src/main.js
@@ -18,3 +18,48 @@ const REQUEST_CONFIG = {
 };
 
 const getInputValue = id => document.getElementById(id).value;
+
+
+const captureTokenizedInputs = () => ({
+  viewState: getInputValue("__VIEWSTATE"),
+  viewStateGenerator: getInputValue("__VIEWSTATEGENERATOR"),
+  eventValidation: getInputValue("__EVENTVALIDATION")
+});
+
+const captureHiddenInputs = () => ({
+  mjrIdHdn: getInputValue("mjrIdHdn"),
+  fltIdHdn: getInputValue("fltIdHdn"),
+  sgIdHdn: getInputValue("sgIdHdn"),
+  prtTxt: getInputValue("prtTxt")
+});
+
+const formatPayload = ({
+  eventTarget = "",
+  tokenizedInputs = {
+    viewState,
+    viewStateGenerator,
+    eventValidation
+  },
+  hidden = {
+    mjrIdHdn,
+    fltIdHdn,
+    sgIdHdn,
+    prtTxt
+  },
+  thesisId,
+  stdThesisIdLst
+}) =>
+  `__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=${encodeURIComponent(
+    tokenizedInputs.viewState
+  )}&__VIEWSTATEGENERATOR=${encodeURIComponent(
+    tokenizedInputs.viewStateGenerator
+  )}&__EVENTVALIDATION=${encodeURIComponent(
+    tokenizedInputs.eventValidation
+  )}&mjrIdHdn=${hidden.mjrIdHdn}&fltIdHdn=${hidden.fltIdHdn}&sgIdHdn=${
+    hidden.sgIdHdn
+  }&${
+    stdThesisIdLst
+      ? `stdThesisIdLst=${stdThesisIdLst}&rmvBtn=Remove+Thesis`
+      : `thesisIdLst=${thesisId}`
+  }&prtTxt=${hidden.prtTxt}`;
+
