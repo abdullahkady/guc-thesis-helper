4af2f1b08016b19d1178b9fee4dedc158338434e
Abdullah Kady <abdullahkady@gmail.com>
2019-01-11 21:54:31 +0200

    Add initial request structure

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const HEADERS = {
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+  "Accept-language": "en,en-US;q=0.9,ar;q=0.8",
+  "Cache-control": "no-cache",
+  "Content-type": "application/x-www-form-urlencoded",
+  Pragma: "no-cache",
+  "Upgrade-Insecure-Requests": "1"
+};
+
+const REQUEST_CONFIG = {
+  credentials: "include",
+  headers: HEADERS,
+  referrer: "http://student.guc.edu.eg/External/Thesis/ChooseThesis.aspx",
+  referrerPolicy: "no-referrer-when-downgrade",
+  method: "POST",
+  mode: "cors"
+};
+
+const getInputValue = id => document.getElementById(id).value;
