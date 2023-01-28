cad97106e6a1ce0baa46b99d62dda543dc73da13
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-01-28 23:37:15 +0200

    Rewrote postback inject to fix issues when refresh is needed.

--- src/js/inject.js
+++ src/js/inject.js
@@ -158,9 +158,13 @@ const selectionListClone = selectionList.cloneNode(true);
 selectionList.parentNode.appendChild(selectionListClone);
 selectionList.parentNode.removeChild(selectionList);
 
+// Remove the postback method causing random refreshes on selecting an item from the list.
 let myScript = document.createElement('script');
 myScript.setAttribute('type', 'text/javascript');
-myScript.textContent = `__doPostBack = function() {};`;
+myScript.textContent = `oldDoPostBack = __doPostBack; __doPostBack = function(target, args) {
+  if(target == 'thesisIdLst') return;
+  else oldDoPostBack(target, args);
+};`;
 (document.head||document.documentElement).appendChild(myScript);
 
 // ================ADDITION LISTENERS================ //
